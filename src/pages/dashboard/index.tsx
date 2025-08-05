7dbc51728d0180aa3c3ab4a2648767fb79874a3c
Guilherme Hassã <91090230+guilhermehassa@users.noreply.github.com>
2025-08-04 21:20:15 -0300

    Control dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,7 @@
 import styles from './styles.module.css';
+import { GetServerSideProps } from 'next';
+import {getSession} from 'next-auth/react';
+
 export default function Dashboard() {
   return (
     <div className={styles.dashboard}>
@@ -6,4 +9,19 @@ export default function Dashboard() {
       <p>Welcome to your dashboard!</p>
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+  const session = await getSession({req});
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props:{}
+  }
 }
