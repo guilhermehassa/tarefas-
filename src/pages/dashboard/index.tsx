bae56ac1eaa4998fc3d80cab994921808ec9a8d8
Guilherme Hassã <91090230+guilhermehassa@users.noreply.github.com>
2025-08-05 09:58:03 -0300

    Dashboard textarea

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,39 @@
+import { Textarea } from '@/components/textarea';
 import styles from './styles.module.css';
 import { GetServerSideProps } from 'next';
-import {getSession} from 'next-auth/react';
+import {getSession, useSession} from 'next-auth/react';
+import Head from 'next/head';
 
 export default function Dashboard() {
+  const {data: session, status} = useSession();
+
   return (
-    <div className={styles.dashboard}>
-      <h1>Dashboard</h1>
-      <p>Welcome to your dashboard!</p>
-    </div>
+    <>
+      <Head>
+        <title>Tarefas de {session?.user?.name}</title>
+      </Head>
+      <main className={styles.dashboard}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1>Qual sua tarefa?</h1>
+            <form action="#">
+              <Textarea
+                placeholder='Digite aqui sua tarefa...'
+                required
+                autoFocus
+              ></Textarea>
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" id="important" />
+                <label htmlFor="important">Marcar como pública</label>
+              </div>
+              <button type='submit' className={styles.button}>
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
